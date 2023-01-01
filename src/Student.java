a4e481164857fe12062d816d84a2ce73d5d176b6
Hazal Bektas <hazalcennurbektas@gmail.com>
2023-01-01 17:23:00 -0600

    projectadded

--- src/Student.java
+++ src/Student.java
@@ -2,9 +2,16 @@ public class Student {
 
     //COMPLETE THIS CLASS BEFORE MathStudent and ScienceStudent classes as they will be sub-classes of Student class
 
+    public Student(String firstName, String lastName, int age, String gender, String className) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.className = className;
+    }
+
 
     //HERE! Create a 5 args constructor
-
 
 
     /*
@@ -17,6 +24,11 @@ public class Student {
     className
     NOTE: only age will be int and others are String
      */
+    public String firstName;
+    public String lastName;
+    public int age;
+    public String gender;
+    public String className;
 
 
 
@@ -25,12 +37,27 @@ public class Student {
     Provide public getter method for className only
      */
 
+    public String getClassName() {
+        return className;
+    }
 
-
-    /*
+    public void setClassName(String className) {
+        this.className = className;
+    }
+/*
         HERE!
         Override toString() method that returns Student object information
         It should be dynamic considering child classes that inherits
      */
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", className='" + className + '\'' +
+                '}';
+    }
 }
